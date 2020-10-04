dba9867b30b56f5dfe1ba1a3208f4a3a86c6898d
Aniket <aniketindian8@gmail.com>
2020-10-04 10:47:10 +0530

    added desktop notifier
    A notification app when run will show pop ups in desktop

---
+++ notifier.py
@@ -0,0 +1,12 @@
+from plyer import notification
+
+notification.notify(
+    title="Welcome to India",
+    message="India is the best country in the world",
+    app_icon = None,
+    timeout = 10,
+)
+
+# in this time can be added 
+# app icon can be added 
+# when played desktop notification will be shown
