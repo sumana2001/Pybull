84f428c074383495bcfb47169b452f1462e2cb2c
Ramas1998 <srishikasharma1998@gmail.com>
2020-10-08 13:35:02 +0530

    Delete credentials.py

--- twitter-bot/credentials.py
+++
@@ -1,7 +0,0 @@
-# This is just a sample file. You need to  edit this file. You need to get these  details from your Twitter app settings. 
-
-consumer_key = 'LkvC5EpvRxIP8DG2t9t5qFI6T' 
-consumer_secret = 'VgG7osdGOyjzbSvh09ty4AV7dibyr4jZp75tVwZeRDB1ZoLbBv' 
-access_token = '1101768737890263041-lbEAIbyQ0Xd7gKXnQeH5fM5nuAZ0qW' 
-access_token_secret = 'L9IdTcFSxmOfcFyoFvWUkZ5oW8is0orFB13tID9H58ovU'
-
