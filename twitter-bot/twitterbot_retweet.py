71ae9d6bf606c969cafef9fbec4a638ae68cae58
Rishika <srishikasharma1998@gmail.com>
2020-10-08 13:28:07 +0530

    Added twiiter-bot

---
+++ twitter-bot/twitterbot_retweet.py
@@ -0,0 +1,41 @@
+import tweepy 
+from time import sleep 
+from credentials import *
+from config import QUERY, FOLLOW, LIKE, SLEEP_TIME, new_search 
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret) 
+api = tweepy.API(auth) 
+
+print("Twitter bot which retweets, like tweets and follow users") 
+print("Bot Settings") 
+print("Like Tweets :", LIKE) 
+print("Follow users :", FOLLOW) 
+
+for tweet in tweepy.Cursor(api.search, q = new_search, lang="en").items(): 
+	try: 
+		print('\nTweet by: @' + tweet.user.screen_name) 
+		print('\nTweeted at:' + tweet.user.location)
+		tweet.retweet() 
+		print('Retweeted the tweet') 
+
+		# Favorite the tweet 
+		if LIKE: 
+			tweet.favorite() 
+			print('Favorited the tweet') 
+
+		# Follow the user who tweeted 
+		# check that bot is not already following the user 
+		if FOLLOW: 
+			if not tweet.user.following: 
+				tweet.user.follow() 
+				print('Followed the user') 
+
+		sleep(SLEEP_TIME) 
+
+	except tweepy.TweepError as e: 
+		print(e.reason) 
+
+	except StopIteration: 
+		break
+
